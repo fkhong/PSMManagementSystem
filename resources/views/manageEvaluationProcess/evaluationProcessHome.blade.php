1fd816941a4333badcec6916c333ea3640b973ff
Foong KH <64588731+fkhong@users.noreply.github.com>
2022-06-04 19:30:49 +0800

    update 1

--- resources/views/manageEvaluationProcess/evaluationProcessHome.blade.php
+++ resources/views/manageEvaluationProcess/evaluationProcessHome.blade.php
@@ -1,16 +1,21 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Evaluation Process') }}
-        </h2>
-    </x-slot>
+@extends('layouts.psmCalculation')
+@section('content')
+    
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <h1> Evaluation Process</h1>
-                
-            </div>
-        </div>
+    <div class="mt-8 text-2xl">
+    View Industrial Evaluation Schedule
+    
     </div>
-</x-app-layout>
+
+    <div class="mt-6 text-gray-500">
+    
+    </div>
+<!-- EDIT HERE-->
+    
+    
+    
+                
+        
+
+    
+@endsection
