84afbc1fcc95e4fafd33f4bfbd21af2ad87dae73
milunnie99 <106214068+milunnie99@users.noreply.github.com>
2022-06-07 23:31:47 +0800

    Update evaluationProcessHome.blade.php
    Home Page - Manage Evaluation Process

--- resources/views/manageEvaluationProcess/evaluationProcessHome.blade.php
+++ resources/views/manageEvaluationProcess/evaluationProcessHome.blade.php
@@ -1,21 +1,27 @@
-@extends('layouts.psmCalculation')
+@extends('layouts.psmEvaluation')
 @section('content')
     
 
     <div class="mt-8 text-2xl">
-    View Industrial Evaluation Schedule
-    
+    Manage Evaluation
     </div>
 
     <div class="mt-6 text-gray-500">
+    <div style="font-size : 25px">
+
+</div>
+    <a href="/addEvaluation"><button class="button">Add Evaluation</button></a>&nbsp&nbsp&nbsp
+    <a href="/viewEvaluation"><button class="button">View Evaluation</button></a>
     
     </div>
-<!-- EDIT HERE-->
+    &nbsp&nbsp&nbsp
     
     
-    
-                
-        
 
-    
+    <div class="bg-gray-200 bg-opacity-25 grid grid-cols-1 md:grid-cols-2">
+        <div class="p-6">
+        
+        </div> 
+    </div>
+
 @endsection
