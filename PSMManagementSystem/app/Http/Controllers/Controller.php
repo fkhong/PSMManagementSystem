4a46485fe1840ced5eb21d2663a62fea206378b7
LINLINA88 <74412617+LINLINA88@users.noreply.github.com>
2022-06-10 02:15:37 +0800

    reminderForm,reminderList

--- PSMManagementSystem/app/Http/Controllers/Controller.php
+++ PSMManagementSystem/app/Http/Controllers/Controller.php
@@ -2,12 +2,43 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use Illuminate\Foundation\Bus\DispatchesJobs;
-use Illuminate\Foundation\Validation\ValidatesRequests;
-use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Http\Requests;
+use Carbon\Carbon;
+use App\Models\evaluationReminder;
+use App\Models\Evaluation;
+use Illuminate\Support\Facades\AuthorizesRequests;
 
-class Controller extends BaseController
+class ReminderController extends Controller
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+   public function test(){
+       return view ('manageReminder/test');
+   }
+
+   public function reminderForm(){
+       $staffID = evaluationReminder::where ('staffID','='requast('staffID'))->first();
+
+       if ($staffID == null){
+           $evaluationReminder = new evaluationReminder();
+           $evaluationReminder-> reminderID = requast ('reminderID');
+           $evaluationReminder-> staffID = requast ('staffID');
+           $evaluationReminder-> evaluationPhase = requast ('evaluationPhase');
+           $evaluationReminder-> staffPosition = requast ('staffPosition');
+           $evaluationReminder-> ieDate = requast ('ieDate');
+           $evaluationReminder-> ieTime = requast ('ieTime');
+
+           $evaluationReminder->save();
+           return redirect('/reminderList')->with('status','Successfully save reminder');
+
+       }
+
+   }
+   public function reminderList(){
+        $data = $evaluationReminder::all();
+
+        $reminderForm = evaluationReminder::where ('staffID','=',$staffID)->value(reminderForm)
+        return view ('manageReminder/reminderList',['items'=>$data]);
+   }
+   {
+   public function  
+   }
 }
