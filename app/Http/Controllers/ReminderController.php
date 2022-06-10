97aefa31089a36b8bba2083c66813d86bc8ca165
LINLINA88 <74412617+LINLINA88@users.noreply.github.com>
2022-06-10 13:29:52 +0800

    reminderController

--- app/Http/Controllers/ReminderController.php
+++ app/Http/Controllers/ReminderController.php
@@ -19,6 +19,50 @@ class ReminderController extends Controller
         }
         
     }
+    public function test(){
+        return view ('manageReminder/test');
+    }
+ 
+    public function reminderForm(){
+ 
+         $evaluationReminder = evaluationReminder::select('staffID')->get();
+         $data = evaluationReminder::whereNotIn('staffID',$evaluationReminder)->get()->sortByAssc('reminderForm')->take (50);
+ 
+        $staffID = evaluationReminder::where ('staffID','='requast('staffID'))->first();
+ 
+        if ($staffID == null){
+            $evaluationReminder = new evaluationReminder();
+            $evaluationReminder-> reminderID = requast ('reminderID');
+            $evaluationReminder-> staffID = requast ('staffID');
+            $evaluationReminder-> evaluationPhase = requast ('evaluationPhase');
+            $evaluationReminder-> staffPosition = requast ('staffPosition');
+            $evaluationReminder-> ieDate = requast ('reminderDate');
+            $evaluationReminder-> ieTime = requast ('reminderTime');
+ 
+            $evaluationReminder->save();
+            return redirect('/reminderList')->with('status','Successfully save reminder');
+ 
+        }
+ 
+    }
+    public function reminderList(){
+         $data = $evaluationReminder::all();
+ 
+         $reminderForm = evaluationReminder::where ('staffID','=',$staffID)->value(reminderForm)
+         return view ('manageReminder/reminderList',['items'=>$data]);
+    }
+    {
+    public function reminderEdit(){
+        $reminder_edit = evaluationReminder::select('*')->where('reminderID','=',$reminderID)->first();
+         return view('manageReminder/reminderEdit'),['items'=>$reminder_edit]);
+ 
+     } 
+    }
+    public function reminderDelete(){
+        $data = evaluationReminder::select('*')->where('reminderID','=',$reminderID)->delete();
+        return redirect('/reminderList')->with('status','Successfully delete reminder');
+ 
+    }
 
 
 }
